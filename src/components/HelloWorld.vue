84279d46f04e3117be8b2ff5be8b67bd83dd642e
Paula Ravas <paula@Paulas-MacBook-Air.local>
2024-03-06 16:05:57 +0200

    Scss config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="hello1">
+    <div class="wrapper">
+      <div class="middle-part">
+        <div class="card-container">
+          <!-- First row of cards -->
+          <div class="card">Air Quality<span class="card-text">XX</span></div>
+          <div class="card">Precipitation</div>
+          <div class="card">Wind</div>
+          <!-- Second row of cards -->
+          <div class="card">Pressure</div>
+          <div class="card">Visibility</div>
+          <div class="card">UV Index</div>
+        </div>
+      </div>
+    <div class="weather-box">
+      <div class="box">
+        <div class="info">
+          <div class="weather">
+            <h1 class="city-name">Sibiu, Romania</h1>
+            <div class="city-temperature">
+              <img :src="require('@/style/images/day/116.png')">
+              <div>
+                <p class="temperature">20<span>C</span></p>
+                <p class="temp-feel">Feels like:</p>
+              </div>
+              
+              <span>15C</span>
+              <p class="high-low">H: <span>XX</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="additional-card">Additional Card</div>
+    </div>
+  </div>
+  <h1>{{ msg }}</h1>
+  
+ <!-- <div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -27,10 +65,16 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    </div>
+  -->
   </div>
+
 </template>
 
 <script>
+
+/*import Header from './components/Header.vue' */
+
 export default {
   name: 'HelloWorld',
   props: {
